da8f3e162baf984b8442f1e720310ce764326964
Oleh Nahorniak <nagornyak68@gmail.com>
2023-09-03 17:03:09 +0300

    use jsoup lib instead of regex

--- src/main/java/com/aliezkan/pageparser/service/PageParserService.java
+++ src/main/java/com/aliezkan/pageparser/service/PageParserService.java
@@ -4,12 +4,13 @@ import com.aliezkan.pageparser.exception.TableNotFoundException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -17,16 +18,34 @@ import java.util.regex.Pattern;
 public class PageParserService {
 
     private final HtmlFetchService htmlFetchService;
-    private static final Pattern TABLE_PATTERN = Pattern.compile("<table[^>]*>(.*?)</table>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-
     public Mono<String> parse(@NonNull String url) {
         return htmlFetchService.fetchHtml(url)
-                .map(html -> {
-                    Matcher matcher = TABLE_PATTERN.matcher(html);
-                    return matcher.find() ? matcher.group(0) : "";
-                })
+                .map(this::getTableFromHtml)
+                .map(this::formatTableWithStyles)
                 .filter(StringUtils::isNotBlank)
                 .switchIfEmpty(Mono.error(new TableNotFoundException()))
                 .cache();
+    }
+
+    private Elements getTableFromHtml(String html) {
+        Document document = Jsoup.parse(html);
+        return document.select("table");
+    }
+
+    private String formatTableWithStyles(Elements tableElements) {
+        if (tableElements.size() > 0) {
+            Element table = tableElements.get(0);
+
+            table.attr("class", "beautiful-table");
+            table.attr("style", "border-collapse: collapse; width: 100%;");
+
+            Elements tableHeaders = table.select("th");
+            Elements tableCells = table.select("td");
+
+            tableHeaders.attr("style", "background-color: #f2f2f2; padding: 8px; text-align: left;");
+            tableCells.attr("style", "border: 1px solid #ddd; padding: 8px; text-align: left;");
+        }
+
+        return tableElements.toString();
     }
 }
